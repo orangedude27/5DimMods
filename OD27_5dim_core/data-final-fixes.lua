e130b0031466bfd26ddbd845be24a104fb15c088
Simon Brodtmann <simon.brodtmann@nexmart.com>
2024-02-11 23:02:35 +0100

    Don't increase stack size of 1
    https://discord.com/channels/1174743568590590053/1205957734533701672

--- OD27_5dim_core/data-final-fixes.lua
+++ OD27_5dim_core/data-final-fixes.lua
@@ -4,7 +4,9 @@ require("prototypes.item-group-changes")
 --Stack changes
 if settings.startup["5d-change-stack"] then
     for _, item in pairs(data.raw.item) do
-        item.stack_size = item.stack_size * settings.startup["5d-change-stack"].value
+        if (item.stack_size > 1) then
+            item.stack_size = item.stack_size * settings.startup["5d-change-stack"].value
+        end
     end
     for _, item in pairs(data.raw.ammo) do
         item.stack_size = item.stack_size * settings.startup["5d-change-stack"].value
